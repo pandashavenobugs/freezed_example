037854e14f8e5f7299333827325807df0ca446f3
cengiz berat dinckan <cedinckan@gmail.com>
2022-01-25 22:37:14 +0300

    json factory added

--- lib/models/job/job.model.dart
+++ lib/models/job/job.model.dart
@@ -1,10 +1,13 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'job.model.freezed.dart';
+part 'job.model.g.dart';
 
 @freezed
 class Job with _$Job {
   const Job._(); // to be able to use custom methods
   const factory Job({@Default("developer") String? title, required int level}) =
       _Job;
+
+  factory Job.fromJson(Map<String, dynamic> json) => _$JobFromJson(json);
 }
